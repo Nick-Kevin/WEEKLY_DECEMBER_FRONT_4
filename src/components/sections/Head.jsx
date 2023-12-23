e5737aa2536a4232c22b4fa3906b9f9f4b369b6b
Nick-Kevin <nicblou23@gmail.com>
2023-12-23 10:39:28 +0300

    update head component

--- src/components/sections/Head.jsx
+++ src/components/sections/Head.jsx
@@ -1,10 +1,15 @@
 import dashboard from '../../assets/Sections/Head/screens.png'
+import Fade from 'react-reveal/Fade'
 
 function Head () {
     return (
         <div id="head" className="bg-dark pt-12 lg:pt-20 h-auto">
             <div className='flex flex-col items-center'>
-                <h1 className='f-medium text-3xl lg:text-7xl w-7/12 lg:w-6/12'>Work at the speed of thought</h1>
+                <Fade bottom>
+                    <h1 className='f-medium text-3xl lg:text-7xl w-7/12 lg:w-6/12'>
+                        Work at the speed of thought
+                    </h1>
+                </Fade>                
             </div>
             <div className="w-full flex justify-center lg:pb-20 relative">
                 <img src={dashboard} alt="dashboard" />
