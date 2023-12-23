32b62670d9b5be5138b09627513e76e98bf466c0
Nick-Kevin <nicblou23@gmail.com>
2023-12-23 11:34:12 +0300

    update Head component

--- src/components/sections/Head.jsx
+++ src/components/sections/Head.jsx
@@ -10,16 +10,18 @@ function Head () {
                         Work at the speed of thought
                     </h1>
                 </Fade>
-                <Fade top>
+                <Fade bottom>
                     <p className='f-regular text-xs leading-normal lg:text-lg w-56 lg:w-5/12 mt-5 lg:mt-8'>
                         Most calendars are designed for teams. Slate is designed for 
                         freelancers who want a simple way to plan their schedule.
                     </p>
                 </Fade>
+                <Fade bottom>
                 <div className='flex flex-col lg:flex-row items-center mt-12 lg:mt-20 mb-8 gap-y-4 lg:gap-y-0 lg:gap-x-8'>
                     <button className='bg-primary f-medium w-28 lg:w-40 h-10 lg:h-14 text-xs lg:text-md hover:border-white hover:border-1 active:underline text-sm'>Try For Free</button>
                     <button className='bg-transparent f-medium w-28 lg:w-40 h-10 lg:h-14 text-xs lg:text-md hover:bg-white active:underline hover:text-black border-white border-1'>Learn More</button>
-                </div>                      
+                </div>
+                </Fade>                   
             </div>
             <div className="w-full flex justify-center lg:pb-20 relative">
                 <img src={dashboard} alt="dashboard" />
