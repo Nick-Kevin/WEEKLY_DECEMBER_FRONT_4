376422336c1c16dac782a41d81b893f6c84d2f6a
Nick-Kevin <nicblou23@gmail.com>
2023-12-23 12:50:23 +0300

    update features components

--- src/components/sections/Features.jsx
+++ src/components/sections/Features.jsx
@@ -6,6 +6,15 @@ function Features () {
             <Fade bottom>
                 <h2 className="mt-20 lg:mt-32 f-regular text-2xl lg:text-5xl">FEATURES</h2>
             </Fade>
+            <div className="flex justify-center">
+                <Fade bottom>
+                    <p className="mt-4 lg:mt-8 leading-relaxed lg:leading-normal f-regular text-xs lg:text-lg w-60 lg:w-5/12">
+                        Most calendars are designed for teams. 
+                        Slate is designed for freelancers who want 
+                        a simple way to plan their schedule.
+                    </p>
+                </Fade>
+            </div>
         </>
     )
 }
