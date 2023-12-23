0f30b87a6287462417ca6bb3968dd8eefe6aa9a7
Nick-Kevin <nicblou23@gmail.com>
2023-12-23 11:00:44 +0300

    update head component

--- src/components/sections/Head.jsx
+++ src/components/sections/Head.jsx
@@ -3,13 +3,17 @@ import Fade from 'react-reveal/Fade'
 
 function Head () {
     return (
-        <div id="head" className="bg-dark pt-12 lg:pt-20 h-auto">
+        <div id="head" className="bg-dark pt-9 lg:pt-20 h-auto">
             <div className='flex flex-col items-center'>
                 <Fade bottom>
-                    <h1 className='f-medium text-3xl lg:text-7xl w-7/12 lg:w-6/12'>
+                    <h1 className='f-medium lg:!leading-[1.07] text-3xl lg:text-7xl w-7/12 lg:w-6/12'>
                         Work at the speed of thought
                     </h1>
-                </Fade>                
+                </Fade>
+                <p className='f-regular text-xs leading-normal lg:text-lg w-56 lg:w-5/12 mt-5 lg:mt-8'>
+                    Most calendars are designed for teams. Slate is designed for 
+                    freelancers who want a simple way to plan their schedule.
+                </p>            
             </div>
             <div className="w-full flex justify-center lg:pb-20 relative">
                 <img src={dashboard} alt="dashboard" />
