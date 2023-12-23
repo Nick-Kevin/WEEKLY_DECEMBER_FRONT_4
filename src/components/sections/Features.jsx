b1de0bc51eba7f772737dcd24b7f6197211c3536
Nick-Kevin <nicblou23@gmail.com>
2023-12-23 12:34:48 +0300

    create Features component

---
+++ src/components/sections/Features.jsx
@@ -0,0 +1,13 @@
+import { Fade } from "react-reveal"
+
+function Features () {
+    return (
+        <>
+            <Fade bottom>
+                <h2 className="mt-20 lg:mt-32 f-regular text-2xl lg:text-5xl">FEATURES</h2>
+            </Fade>
+        </>
+    )
+}
+
+export default Features
