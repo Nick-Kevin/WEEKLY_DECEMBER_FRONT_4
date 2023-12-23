f4c68557eeb300fd1c3a957901bb40e6fd52d24b
Nick-Kevin <nicblou23@gmail.com>
2023-12-23 09:26:02 +0300

    update navbar

--- src/components/header/StickyNavbar2.jsx
+++ src/components/header/StickyNavbar2.jsx
@@ -69,39 +69,41 @@ export default function StickyNavbar2() {
       <div className="flex items-center justify-between text-blue-gray-900">
         <img src={darkLogo} className="w-28 lg:w-44" alt="dark logo" />
         <div className="hidden lg:block">{navList}</div>
-        <div className="flex items-center gap-x-1">
-          <Button
-            variant="gradient"
-            className="inline-block text-xs lg:text-sm border-white border-1 px-7 lg:px-10 py-2 lg:py-3"
-          >
-            <span className="f-medium">Login</span>
-          </Button>
-        </div>
-        <IconButton
-          variant="text"
-          className="w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-          ripple={false}
-          onClick={() => setOpenNav(!openNav)}
-        >
-          {openNav ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              className="h-6 w-6"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
+        <div className="flex">
+          <div className="flex items-center gap-x-1">
+            <Button
+              variant="gradient"
+              className="inline-block text-xs lg:text-sm border-white border-1 px-7 lg:px-10 py-2 lg:py-3 hover:bg-white hover:text-black"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          ) : (
-            <img src={menu} alt="menu"/>
-          )}
-        </IconButton>
+              <span className="f-medium">Login</span>
+            </Button>
+          </div>
+          <IconButton
+            variant="text"
+            className="w-6 text-inherit ml-4 hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+            ripple={false}
+            onClick={() => setOpenNav(!openNav)}
+          >
+            {openNav ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                className="h-6 w-6"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <img src={menu} alt="menu"/>
+            )}
+          </IconButton>
+        </div>
       </div>
       <MobileNav open={openNav}>
         <div className="container mx-auto">
