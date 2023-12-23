73c19c44f34af9bd43fe6050293988aaac7edd6e
Nick-Kevin <nicblou23@gmail.com>
2023-12-23 10:20:08 +0300

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import StickyNavbar2 from './components/header/StickyNavbar2'
-import Flip from 'react-reveal/Flip'
+import Head from './components/sections/Head'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,36 +10,10 @@ function App() {
     <>
       <header className='sticky top-0 z-50'>
         <StickyNavbar2/>
-      </header>      
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>
-        <Flip left cascade>
-          Vite + React
-        </Flip>
-      </h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <h1 className="mt-96 mb-20">
-        <Flip left cascade>
-          Vite + React
-        </Flip>
-      </h1>
+      </header>
+      <main>
+        <Head/>
+      </main>
     </>
   )
 }
