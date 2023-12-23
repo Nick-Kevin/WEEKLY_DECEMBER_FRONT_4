fa70865459c4ee27c2ca9b8ca794319bde44bb2f
Nick-Kevin <nicblou23@gmail.com>
2023-12-23 09:21:00 +0300

    update navbar

--- src/components/header/StickyNavbar2.jsx
+++ src/components/header/StickyNavbar2.jsx
@@ -7,6 +7,7 @@ import {
   IconButton,
 } from "@material-tailwind/react";
 import darkLogo from '../../assets/Logo/logo-dark.png'
+import menu from '../../assets/Header/Vector.svg'
  
 export default function StickyNavbar2() {
   const [openNav, setOpenNav] = React.useState(false);
@@ -66,7 +67,7 @@ export default function StickyNavbar2() {
   return (
     <Navbar className="bg-blue-400 bg-dark px-4 py-2 lg:px-[14.5rem] lg:py-4">
       <div className="flex items-center justify-between text-blue-gray-900">
-        <img src={darkLogo} className="w-32 lg:w-44" alt="dark logo" />
+        <img src={darkLogo} className="w-28 lg:w-44" alt="dark logo" />
         <div className="hidden lg:block">{navList}</div>
         <div className="flex items-center gap-x-1">
           <Button
@@ -78,7 +79,7 @@ export default function StickyNavbar2() {
         </div>
         <IconButton
           variant="text"
-          className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+          className="w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
           ripple={false}
           onClick={() => setOpenNav(!openNav)}
         >
@@ -98,19 +99,7 @@ export default function StickyNavbar2() {
               />
             </svg>
           ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
+            <img src={menu} alt="menu"/>
           )}
         </IconButton>
       </div>
