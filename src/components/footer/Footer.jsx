c4ed1896496ef6a456f2b026ae9261752d87aa22
Nick-Kevin <nicblou23@gmail.com>
2023-12-24 12:16:02 +0300

    add footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,74 @@
+import phone from '../../assets/Footer/phone.svg'
+import map from '../../assets/Footer/map.svg'
+import twitter from '../../assets/Footer/twitter.svg'
+import linkedin from '../../assets/Footer/linkedin.svg'
+import facebook from '../../assets/Footer/facebook.svg'
+
+function Footer () {
+    const categories = [ 'Fingertipe', 'Resources', 'About' ]
+    const links = [ 'Home', 'Examples', 'Pricing', 'Updates' ]
+    const socialNetworks = [ 
+        { image: twitter, alt: 'twitter' },
+        { image: facebook, alt: 'facebook' },
+        { image: linkedin, alt: 'linkedin' }
+     ]
+
+    const linksList = () => {
+        return (
+            <>
+                {
+                    links.map((link, index) => (
+                        <>
+                            <div className={ index === 3 ? '' : 'mb-4' }>
+                                <a
+                                    href="#"
+                                    className='text-white f-medium text-sm'
+                                >
+                                    {link}
+                                </a>
+                            </div>
+                        </>
+                    ))
+                }
+            </>
+        )
+    }
+
+    return (
+        <div className='grid lg:grid-cols-2 lg:gap-x-28 text-white pb-24 lg:mt-12 lg:pl-60'>
+            <div className='grid lg:grid-cols-3'>
+                {
+                    categories.map((category) => (
+                        <div className='flex flex-col justify-start text-start'>
+                            <h6 className='mb-7 text-lg f-medium'>{category}</h6>
+                            {linksList()}
+                        </div>
+                    ))
+                }
+            </div>
+            <div>
+                <div className='flex items-center'>
+                    <img src={map} className='w-11' alt="map logo" />
+                    <p className='ml-1 text-sm f-medium'>7480 Mockingbird Hill undefined </p>
+                </div>
+                <div className='flex items-center mt-3'>
+                    <img src={phone} className='w-8' alt="phone logo" />
+                    <p className='ml-1 text-sm f-medium'>(239) 555-0108</p>
+                </div>
+                <div className='flex gap-x-5 mt-6'>
+                    {
+                        socialNetworks.map((item) =>(
+                            <>
+                                <a href="#">
+                                    <img src={item.image} className='w-10' alt={item.alt} />
+                                </a>
+                            </>
+                        ))
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
