28288546bb1a700004fb62d22253b23c70782e91
Nick-Kevin <nicblou23@gmail.com>
2023-12-24 14:27:18 +0300

    update head component

--- src/components/sections/Head.jsx
+++ src/components/sections/Head.jsx
@@ -3,29 +3,29 @@ import Fade from 'react-reveal/Fade'
 
 function Head () {
     return (
-        <div id="head" className="pt-9 lg:pt-20 h-auto">
+        <div id="head" className="pt-9 md:pt-16 lg:pt-20 h-auto">
             <div className='flex flex-col items-center'>
                 <Fade bottom>
-                    <h1 className='f-medium lg:!leading-[1.07] text-3xl lg:text-7xl w-7/12 lg:w-6/12'>
+                    <h1 className='f-medium md:!leading-[1.05] lg:!leading-[1.07] text-3xl sm:text-4xl md:text-5xl lg:text-7xl w-7/12 md:w-7/12 lg:w-8/12 xl:w-6/12'>
                         Work at the speed of thought
                     </h1>
                 </Fade>
                 <Fade bottom>
-                    <p className='f-regular text-xs leading-relaxed lg:leading-normal lg:text-lg w-56 lg:w-5/12 mt-5 lg:mt-8'>
+                    <p className='f-regular text-xs md:text-md lg:text-lg leading-relaxed lg:leading-normal w-56 sm:w-6/12 xl:w-5/12 mt-5 lg:mt-8'>
                         Most calendars are designed for teams. Slate is designed for 
                         freelancers who want a simple way to plan their schedule.
                     </p>
                 </Fade>
                 <Fade bottom>
-                <div className='flex flex-col lg:flex-row items-center mt-12 lg:mt-20 mb-8 gap-y-4 lg:gap-y-0 lg:gap-x-8'>
+                <div className='flex flex-col sm:flex-row items-center mt-12 lg:mt-20 mb-8 gap-y-4 lg:gap-y-0 sm:gap-x-5 md:gap-x-8'>
                     <button className='bg-primary f-medium w-28 lg:w-40 h-10 lg:h-14 text-xs lg:text-md hover:border-white hover:border-1 active:underline text-sm'>Try For Free</button>
                     <button className='bg-transparent f-medium w-28 lg:w-40 h-10 lg:h-14 text-xs lg:text-md hover:bg-white active:underline hover:text-black border-white border-1'>Learn More</button>
                 </div>
                 </Fade>                   
             </div>
-            <div className="w-full flex justify-center lg:pb-20 relative">
+            <div className="w-full flex justify-center md:pb-20 relative">
                 <img src={dashboard} alt="dashboard" />
-                <div id="shadow" className="absolute bottom-0 w-full h-16 lg:h-96"></div>
+                <div id="shadow" className="absolute bottom-0 w-full h-16 sm:h-96"></div>
             </div>            
         </div>
     )
