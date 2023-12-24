0bc78f9431fde995ba9d1433ec70a723306748e3
Nick-Kevin <nicblou23@gmail.com>
2023-12-24 16:29:42 +0300

    update gallery component

--- src/components/sections/Gallery.jsx
+++ src/components/sections/Gallery.jsx
@@ -19,24 +19,24 @@ function Gallery () {
     return (
         <div>
             <Fade bottom>
-                <h2 className="mt-32 f-regular text-2xl lg:text-5xl">Gallery</h2>
+                <h2 className="mt-32 f-regular text-2xl sm:text-3xl md:text-4xl lg:text-5xl">Gallery</h2>
             </Fade>
             <div className="flex justify-center">
                 <Fade bottom>
-                    <p className="my-3 lg:mt-8 lg:mb-14 leading-relaxed lg:leading-normal f-regular text-xs lg:text-lg w-56 lg:w-[500px]">
+                    <p className="my-3 lg:mt-8 sm:mb-8 lg:mb-14 leading-relaxed lg:leading-normal f-regular text-xs sm:text-base lg:text-lg w-56 sm:w-11/12 lg:w-[500px]">
                         We focus on ergonomics and meeting you where you work. <br /> 
                         It's only a keystroke away.
                     </p>
                 </Fade>
             </div>
-            <div className="hidden lg:flex justify-center gap-x-8">
+            <div className="hidden sm:flex justify-center gap-x-8">
                 {
                     firstDivOnDesktop.map((image, index) => (
                         <>
                             <Bounce left>
                                 <img
                                     src={image}
-                                    className="rounded-xl h-60 gallery-shadow"
+                                    className="rounded-xl sm:h-36 md:h-40 lg:h-60 gallery-shadow"
                                     alt={'card ' + index}
                                 />
                             </Bounce>                            
@@ -44,7 +44,7 @@ function Gallery () {
                     ))
                 }
             </div>
-            <div className="flex flex-col items-center lg:hidden gap-y-5 mt-4">
+            <div className="flex flex-col items-center sm:hidden gap-y-5 mt-4">
                 {
                     mobileCards.map((image, index) => (
                         <>
@@ -59,14 +59,14 @@ function Gallery () {
                     ))
                 }
             </div>
-            <div className="hidden mt-10 lg:flex justify-center gap-x-8">
+            <div className="hidden mt-10 sm:flex justify-center gap-x-8">
                 {
                     secondDivOnDesktop.map((image, index) => (
                         <>
                             <Bounce left>
                                 <img
                                     src={image}
-                                    className="rounded-xl h-60 gallery-shadow"
+                                    className="rounded-xl sm:h-36 md:h-40 lg:h-60 gallery-shadow"
                                     alt={'card ' + index}
                                 />
                             </Bounce>                            
@@ -75,7 +75,7 @@ function Gallery () {
                 }
             </div>
             <Fade bottom>
-                <button className='bg-transparent f-medium w-28 lg:w-36 h-10 lg:h-12 gallery-shadow text-xs lg:text-lg hover:bg-white active:underline hover:text-black border-white border-1 my-7 lg:my-10'>
+                <button className='bg-transparent f-medium w-28 lg:w-36 h-10 lg:h-12 gallery-shadow text-xs lg:text-lg hover:bg-white active:underline hover:text-black border-white border-1 my-7 sm:my-9 lg:my-10'>
                     See more
                 </button>
             </Fade>
