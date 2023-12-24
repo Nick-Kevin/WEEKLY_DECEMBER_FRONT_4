1970e0bd7f0c1dd40133689e32f8abe0710f0573
Nick-Kevin <nicblou23@gmail.com>
2023-12-24 09:02:26 +0300

    update images name in testimonials folder

---
+++ src/components/sections/Testimonials.jsx
@@ -0,0 +1,13 @@
+import { Fade } from "react-reveal"
+
+function Testimonials () {
+    return (
+        <div>
+            <Fade bottom>
+                <h2 className="mt-28 lg:mt-40 f-regular text-2xl lg:text-5xl">Testimonials</h2>
+            </Fade>
+        </div>
+    )
+} 
+
+export default Testimonials
