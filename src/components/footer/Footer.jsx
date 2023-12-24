8b170ea71f7cb3aa1f67c00250cd7b4de307466c
Nick-Kevin <nicblou23@gmail.com>
2023-12-24 12:28:07 +0300

    update footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -19,10 +19,10 @@ function Footer () {
                 {
                     links.map((link, index) => (
                         <>
-                            <div className={ index === 3 ? '' : 'mb-4' }>
+                            <div className={ index === 3 ? 'mb-12 lg:mb-0' : 'mb-1 lg:mb-4' }>
                                 <a
                                     href="#"
-                                    className='text-white f-medium text-sm'
+                                    className='text-white f-medium text-xs lg:text-sm'
                                 >
                                     {link}
                                 </a>
@@ -35,12 +35,12 @@ function Footer () {
     }
 
     return (
-        <div className='grid lg:grid-cols-2 lg:gap-x-28 text-white pb-24 lg:mt-12 lg:pl-60'>
+        <div className='grid lg:grid-cols-2 lg:gap-x-28 text-white pb-24 mt-12 mx-12 lg:mx-0 lg:pl-60'>
             <div className='grid lg:grid-cols-3'>
                 {
                     categories.map((category) => (
                         <div className='flex flex-col justify-start text-start'>
-                            <h6 className='mb-7 text-lg f-medium'>{category}</h6>
+                            <h6 className='mb-6 lg:mb-7 text-sm lg:text-lg f-medium'>{category}</h6>
                             {linksList()}
                         </div>
                     ))
@@ -48,19 +48,19 @@ function Footer () {
             </div>
             <div>
                 <div className='flex items-center'>
-                    <img src={map} className='w-11' alt="map logo" />
-                    <p className='ml-1 text-sm f-medium'>7480 Mockingbird Hill undefined </p>
+                    <img src={map} className='w-7 lg:w-11' alt="map logo" />
+                    <p className='ml-1 text-xs lg:text-sm f-medium'>7480 Mockingbird Hill undefined </p>
                 </div>
                 <div className='flex items-center mt-3'>
-                    <img src={phone} className='w-8' alt="phone logo" />
-                    <p className='ml-1 text-sm f-medium'>(239) 555-0108</p>
+                    <img src={phone} className='w-6 lg:w-8' alt="phone logo" />
+                    <p className='ml-1 text-xs lg:text-sm f-medium'>(239) 555-0108</p>
                 </div>
-                <div className='flex gap-x-5 mt-6'>
+                <div className='flex gap-x-4 lg:gap-x-5 mt-6'>
                     {
                         socialNetworks.map((item) =>(
                             <>
                                 <a href="#">
-                                    <img src={item.image} className='w-10' alt={item.alt} />
+                                    <img src={item.image} className='w-6 lg:w-10' alt={item.alt} />
                                 </a>
                             </>
                         ))
